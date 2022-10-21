f406d1bc623c6e83a64da164eb96487ed57ad037
Fábio Chiquezi <fabiochiquezi@gmail.com>
2022-10-21 12:37:24 -0300

    next new config structure

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
